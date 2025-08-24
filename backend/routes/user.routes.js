c35de6f4fc760b02ae26ede15dd6a6c9fc75364c
Arfat Fakih <devilsevil92@gmail.com>
2025-08-24 23:26:51 +0530

    User Connections routes are created

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { downloadProfile, getAllUserProfile, getUserAndProfile, login, regitser, updateProfileData, updateUserProfile, uploadProfilePicture } from "../controllers/user.controller.js";
+import { acceptConnectionRequest, allMyConnectionRequest, downloadProfile, getAllUserProfile, getMyConnectionsRequests, getUserAndProfile, login, regitser, sendConnectionRequest, updateProfileData, updateUserProfile, uploadProfilePicture } from "../controllers/user.controller.js";
 import multer from "multer";
 
 const router = Router();
@@ -24,5 +24,9 @@ router.route('/get_user_and_profile').get(getUserAndProfile);
 router.route('/update_profile_data').post(updateProfileData);
 router.route('/user/get_all_users').get(getAllUserProfile);
 router.route('/user/download_resume').get(downloadProfile);
+router.route('/user/send_connection_request').post(sendConnectionRequest);
+router.route('/user/getConnectionRequests').get(getMyConnectionsRequests);
+router.route('/user/user_connection_request').get(allMyConnectionRequest);
+router.route('/user/accept_connection_request').post(acceptConnectionRequest);
 
 export default router;
