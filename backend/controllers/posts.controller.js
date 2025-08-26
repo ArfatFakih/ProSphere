e7c5e758bcd5752a5a8dbc6c7b8aad4ba19e7a70
Arfat Fakih <devilsevil92@gmail.com>
2025-08-26 12:35:31 +0530

    All posts related routes are created

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,6 +1,7 @@
 import express from "express"   
 import User from "../models/user.model.js";
 import Post from "../models/posts.model.js";
+import Comment from "../models/comments.model.js";
 
 const activeCheck = async (req, res) => {
     return res.status(200).json({ message: "RUNNING"});
@@ -33,7 +34,161 @@ const createPost = async (req, res) => {
 }
 
 
+const getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find().populate('userId', 'name username email profilePicture');
+        return res.status(200).json({ posts });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }  
+}
+
+
+const deletePost = async (req, res) => {
+    const { token, post_id } = req.body;
+
+    try {
+
+        const user = await User.findOne({ token: token }).select('_id');
+
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        
+        const post = await Post.findOne({ _id: post_id });
+
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        if(post.userId.toString() !== user._id.toString()) {
+            return res.status(403).json({ message: "You are not authorized to delete this post" });
+        }
+
+        await Post.deleteOne({ _id: post_id });
+
+        return res.status(200).json({ message: "Post deleted successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+const commentPost = async (req, res) => {
+
+    const { token, post_id, commentBody } = req.body;
+
+    try {
+
+        const user = await User.findOne({ token: token }).select('_id');
+
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const post = await Post.findOne({ _id: post_id });
+
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        
+        const comment = new Comment({
+            userId: user._id,
+            postId: post._id,
+            body: commentBody
+        });
+
+        await comment.save();
+
+        return res.status(201).json({ message: "Comment added successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+const get_comments_by_post = async (req, res) => {
+    
+    const { post_id } = req.body;
+
+    try {
+        const post = await Post.findOne({ _id: post_id });
+        
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        return res.status(200).json({ comments: post.comments})
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
+const delete_comment_of_user = async (req, res) => {
+    
+    const { token, comment_id } = req.body;
+    
+    try {
+
+        const user = await User.findOne({ token: token }).select('_id');
+
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        
+        const comment = await Comment.findOne({ _id: comment_id });
+
+        if(!comment) {
+            return res.status(404).json({ message: "Comment not found" });
+        }
+
+        if(comment.userId.toString() !== user._id.toString()) {
+            return res.status(403).json({ message: "You are not authorized to delete this comment" });
+        }
+
+        await Comment.deleteOne({ _id: comment_id });
+
+        return res.status(200).json({ message: "Comment deleted successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
+const increment_likes = async (req, res) => {
+    
+    const { post_id } = req.body;
+
+    try {
+        
+        const post = await Post.findOne({ _id: post_id });
+
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        post.likes = post.likes + 1;
+
+        await post.save();
+
+        return res.status(200).json({ message: "Likes incremented successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
 export {
     activeCheck,
-    createPost
+    createPost,
+    getAllPosts,
+    deletePost,
+    commentPost,
+    get_comments_by_post,
+    delete_comment_of_user,
+    increment_likes
 }
