cf2f3323d2f76b2318544d8b1bfab16990a80895
Arfat Fakih <devilsevil92@gmail.com>
2025-08-27 17:15:40 +0530

    Landing page is Created

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+import UserLayout from "@/layout/UserLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,9 +16,29 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
-    <>
-      <h1> Hello </h1>
-    </>
+    <UserLayout>
+      <div className= {styles.container}>
+        <div className={styles.mainContainer}>
+          <div className={styles.mainContainer_left}>
+            <p>Connect with Friends without Exaggeration</p>
+            <p>A True social media platform, with stories no blufs !</p>
+
+            <div onClick={()=> { router.push("/login") }} className={styles.buttonJoin}>
+              <p>Join now</p>
+            </div>
+          </div>
+
+          <div className={styles.mainContainer_right}>
+            <img src="/images/homemain_pic.png" alt="home_pic" />
+          </div>
+
+        </div>
+
+      </div>
+    </UserLayout>
   );
 }
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import UserLayout from '@/layout/UserLayout'
+import React from 'react'
+
+function LoginComponent() {
+  return (
+    <UserLayout>
+      <h1>Login</h1>
+    </UserLayout>
+  )
+}
+
+export default LoginComponent
