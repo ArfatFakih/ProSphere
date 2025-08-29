51bfe94ad05638080d3fbead44dd94610d13175c
Arfat Fakih <devilsevil92@gmail.com>
2025-08-29 20:47:17 +0530

    On Dashboard Page create post design is created

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -27,19 +27,51 @@ const Dashboard = () => {
 
     },[authState.isTokenThere])
 
-  return (
-    <UserLayout>
-      <DashboardLayout>
-        <div className={styles.scrollComponent}>
 
-          <div className={styles.createPostContainer}>
-            <img width={100} src={`${BASE_URL}/${authState.user.userId.profilePicture}`} alt="" />
-          </div>
+    const [postContent, setPostContent] = useState("");
+    const [fileContent, setFileContent] = useState();
 
-        </div>
-      </DashboardLayout>
-    </UserLayout>
-  )
+    if(authState.user) {
+        return (
+          <UserLayout>
+            <DashboardLayout>
+              <div className={styles.scrollComponent}>
+
+                <div className={styles.createPostContainer}>
+                  <img className={styles.userProfile} src={`${BASE_URL}/${authState.user.userId.profilePicture}`} alt="" />
+                  <textarea onChange={(e) => setPostContent(e.target.value)} value={postContent} placeholder={"What's in your mind"} className={styles.textAreaOfContent} name="" id=""></textarea>
+                  <label htmlFor="fileUpload">
+                    <div className={styles.Fab}>
+
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                      </svg>
+
+                    </div>
+                  </label>
+                  <input onChange={(e) => setFileContent(e.target.files[0])} type="file" hidden id="fileUpload" />
+
+                  {postContent.length > 0 && 
+                  <div className={styles.uploadButton}>Post</div> 
+                  }
+                  
+                </div>
+
+              </div>
+            </DashboardLayout>
+          </UserLayout>
+        )
+    }else {
+      return (
+        <UserLayout>
+          <DashboardLayout>
+            <div>
+              <h1>Loading...</h1>
+            </div>
+          </DashboardLayout>
+        </UserLayout>
+      )
+    }
 }
 
 export default Dashboard
